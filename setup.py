5e41e9ac0e3a8893155157269e85023bd913b365
Kasra Koushan <kasrakoushan@khanacademy.org>
2017-06-13 16:01:44 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='google_appengine',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['google'],
+      zip_safe=False)
